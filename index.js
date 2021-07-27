223912fe19dceffadfa121cef083f2cc3cadf05b
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-07-27 21:18:47 +0200

    get /products

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose
+  .connect('mongodb://localhost:27017/udemy-farm', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!!!');
+  })
+  .catch((err) => {
+    console.log('OH NO MONGO CONNECTION ERROR!!!!');
+    console.log(err);
+  });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
+});
+
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
 });
 
 app.listen(3000, () => {
