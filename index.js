a81fcf093f1224a0b7abce00ae0b1edc93866f85
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-09-13 22:00:50 +0200

    /products/:id/edit form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override');
 const Product = require('./models/product');
 
 const categories = ['fruit', 'vegetable', 'dairy'];
@@ -24,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
@@ -49,6 +50,19 @@ app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product, categories });
+});
+
+app.put('/products/:id', async(req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+  console.log(req.body);
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
