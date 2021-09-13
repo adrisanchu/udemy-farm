3a780c68ccbef865312cbd4250666e32dc0fbb1d
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-09-13 22:08:51 +0200

    DELETE /products/:id routing

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ app.put('/products/:id', async(req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
+app.delete('/products/:id', async(req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect('/products');
+});
+
 app.listen(3000, () => {
   console.log('APP IS LISTENING ON PORT 3000!');
 });
