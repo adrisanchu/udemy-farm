c203d0c5c98a61bb76dd0988e9560c99d49517dc
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-10-07 20:16:30 +0200

    Improve routing: filter products view by category. Populate farms and products.

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ app.get('/farms', async (req, res) => {
   res.render('farms/index', { farms });
 });
 
+app.get('/farms/:id/products/new', async (req, res) => {
+  const { id } = req.params; // the id of the farm!!
+  const farm = await Farm.findById(id);
+  // pass the farm as an object
+  res.render('products/new', { categories, farm });
+});
+
 app.get('/farms/new', (req, res) => {
   res.render('farms/new');
 });
@@ -44,19 +51,42 @@ app.get('/farms/new', (req, res) => {
 app.post('/farms', async (req, res) => {
   const farm = new Farm(req.body);
   await farm.save();
-  // res.redirect('/farms');
   res.redirect(`/farms/${farm._id}`);
+});
+
+app.post('/farms/:id/products', async (req, res) => {
+  const { id } = req.params;
+  // find the farm
+  const farm = await Farm.findById(id);
+  const { name, price, category } = req.body;
+  const product = new Product({ name, price, category });
+  // add the new product into the farms list (multiple products per farm)
+  farm.products.push(product);
+  // add the farm into the new product as well (one product in one farm only)
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${id}`);
 });
 
 app.get('/farms/:id', async (req, res) => {
   const { id } = req.params;
-  const farm = await Farm.findById(id);
+  const farm = await Farm.findById(id).populate('products');
   res.render('farms/show', { farm });
 });
 
 // PRODUCTS
 app.get('/products', async (req, res) => {
-  const products = await Product.find({});
+  const { category } = req.query;
+  let products = {};
+  // check if category is a valid one
+  if (categories.includes(category)) {
+    // filter products by category
+    products = await Product.find({ category: category });
+  } else {
+    // return all products
+    products = await Product.find();
+  }
   res.render('products/index', { products });
 });
 
@@ -73,7 +103,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate('farm', 'name');
   res.render('products/show', { product });
 });
 
